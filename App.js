f3780aec82bb9f5ad1fe82bd99e13e6b929a9cc5
himanshu8987 <94754545+himanshu8987@users.noreply.github.com>
2022-11-05 21:48:21 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+import Navbar from "./components/Navbar/Navbar";
+import Homepage from './components/Homepage/Homepage';
+import TechStack from "./components/TechStack/TechStack";
+//Navbar
+//HomePage
+//Tech Stack
+//Experience
+//Projects 
+
+function App() {
+  return (
+    <div className="app">
+    <Navbar/>
+    <Homepage/>
+    <TechStack/>
+    </div>
+  );
+}
+
+export default App;
